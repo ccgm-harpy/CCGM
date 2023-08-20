a16f51a6bdb70f2dfd25783b42aca3f72b43d892
protochime327 <117958419+ccgm-harpy@users.noreply.github.com>
2023-08-20 00:50:49 -0600

    Config generator update
    Added pywebview to requirements.txt
    Added instructions for the config generator
    Added default renderer to config generator

--- config_generator/generator.py
+++ config_generator/generator.py
@@ -5,4 +5,4 @@ import webview
 app = render_editor()
 server = pn.serve(app, show=False, port=5554, threaded=True)
 webview.create_window('CCGM Config Editor', "http://localhost:5554", width=800, height=800)
-webview.start()
+webview.start(gui="edgechromium")
